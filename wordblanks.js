d30858442685a1bf431fddfbb3ef783d387a731e
Hyojin <hyojin.ju@gmx.net>
2022-09-07 14:53:39 +0200

    Word blanks

---
+++ wordblanks.js
@@ -0,0 +1,11 @@
+/*
+A noun, a verb, an adjective and an adverb are provided. 
+A complete sentence needs to be formed using those words.
+*/
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
